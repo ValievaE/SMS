8a8ced77fd44e147f3c0da66279adb3012985290
Elena Valieva <valieva-1991@mail.ru>
2020-09-22 21:54:07 +0300

    доработка 21.09.2020_2

--- app/src/main/java/com/example/sms/MainActivity.java
+++ app/src/main/java/com/example/sms/MainActivity.java
@@ -12,6 +12,7 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
