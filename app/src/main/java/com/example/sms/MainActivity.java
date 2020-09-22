ff65c118b15908c62599c92c5382d374295688dd
Elena Valieva <valieva-1991@mail.ru>
2020-09-22 21:51:30 +0300

    доработка 21.09.2020

--- app/src/main/java/com/example/sms/MainActivity.java
+++ app/src/main/java/com/example/sms/MainActivity.java
@@ -13,46 +13,60 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int CALL_PHONE_PERMISSION = 10;
     private static final int SMS_PERMISSION = 11;
-    
-
-    private Button buttonCall;
-    private Button buttonSend;
+    private String numberPhone = "";
     private EditText editTextPhone;
     private EditText editTextSms;
-    private String numberPhone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonCall = findViewById(R.id.buttonCall);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextSms = findViewById(R.id.editTextSms);
+
+        Button buttonCall = findViewById(R.id.buttonCall);
         buttonCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 call();
+
             }
         });
 
-        buttonSend = findViewById(R.id.buttonSend);
+        Button buttonSend = findViewById(R.id.buttonSend);
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendSMS();
+
             }
         });
 
     }
 
+
+
+    private void sendSMS() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION);
+        } else {
+            String message = editTextSms.getText().toString();
+            SmsManager smgr = SmsManager.getDefault();
+            smgr.sendTextMessage(numberPhone, null, message, null, null);
+        }
+
+    }
     private void call() {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, CALL_PHONE_PERMISSION);
         } else {
-            editTextPhone = findViewById(R.id.editTextPhone);
             numberPhone = "tel:" + editTextPhone.getText().toString();
             Uri uri = Uri.parse(numberPhone);
             Intent intent = new Intent(Intent.ACTION_CALL, uri);
@@ -60,39 +74,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void sendSMS() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION);
-        } else {
-            editTextSms = findViewById(R.id.editTextSms);
-            String message = editTextSms.getText().toString();
-            SmsManager smgr = SmsManager.getDefault();
-            smgr.sendTextMessage(numberPhone, null, message, null, null);
-        }
-
-    }
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permission, int[] grandResults) {
         switch (requestCode) {
-            case CALL_PHONE_PERMISSION: {
-                if (grandResults.length > 0 && grandResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    call();
-                } else {
-                    finish();
-                }
-                break;
-            }
-
             case SMS_PERMISSION: {
                 if (grandResults.length > 0 && grandResults[0] == PackageManager.PERMISSION_GRANTED) {
                     sendSMS();
                 } else {
+                    Toast.makeText(MainActivity.this, "Вы не дали разрешения на отправку SMS", Toast.LENGTH_SHORT).show();
                     finish();
                 }
                 break;
             }
+            case CALL_PHONE_PERMISSION: {
+                if (grandResults.length > 0 && grandResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                } else {
+                    Toast.makeText(MainActivity.this, "Вы не дали разрешения на доступ к звонкам", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            }
+
         }
     }
 
